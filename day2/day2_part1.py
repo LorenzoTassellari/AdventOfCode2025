9dd6a4c2392cbd0ceacae516719967507d12ef1d
Lorenzo Tassellari <m.tassellari@gmail.com>
2025-12-23 17:24:23 +0000

    First really dirty correct answer

--- day2/day2_part1.py
+++ day2/day2_part1.py
@@ -1,5 +1,27 @@
-with open("day2/test.txt", 'r') as f:
+with open("day2/input.txt", 'r') as f:
     exercise_input = f.read()
-print(exercise_input)
 ranges_ids = [ranges.split('-') for ranges in exercise_input.split(',')]
-print(ranges_ids)
+all_ids = []
+for _range in ranges_ids:
+    print("ranges: ", _range)
+    for _id in range(int(_range[0]), int(_range[1]) + 1): 
+        print(_id)
+        all_ids.append(_id)
+
+output = 0
+for _id in all_ids:
+    str_id = str(_id)
+    length_of_id = len(str_id)
+    if length_of_id % 2 == 1: # Get rid of all odd numbers as they can't have a repeated pattern in them.
+        continue
+    first_half_id_index = length_of_id // 2 - 1 # -1 for the first half of the number.
+    id_is_invalid = False
+    for i in range(first_half_id_index + 1):
+        if str_id[i] == str_id[i + first_half_id_index + 1]:
+            id_is_invalid = True
+        else:
+            id_is_invalid = False
+            break
+    if id_is_invalid == True:
+        output += _id
+print(output)
