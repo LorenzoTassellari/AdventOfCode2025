de467b1bcd7da43b1152586f73e11bd3eb7b57bd
Lorenzo Tassellari <m.tassellari@gmail.com>
2025-12-23 17:38:32 +0000

    Cleaned it up a little, still ugly but oh well.

--- day2/day2_part1.py
+++ day2/day2_part1.py
@@ -1,27 +1,40 @@
-with open("day2/input.txt", 'r') as f:
-    exercise_input = f.read()
-ranges_ids = [ranges.split('-') for ranges in exercise_input.split(',')]
-all_ids = []
-for _range in ranges_ids:
-    print("ranges: ", _range)
-    for _id in range(int(_range[0]), int(_range[1]) + 1): 
-        print(_id)
-        all_ids.append(_id)
 
-output = 0
-for _id in all_ids:
+def check_invalid_id(_id : int) -> bool:
+
     str_id = str(_id)
     length_of_id = len(str_id)
-    if length_of_id % 2 == 1: # Get rid of all odd numbers as they can't have a repeated pattern in them.
-        continue
-    first_half_id_index = length_of_id // 2 - 1 # -1 for the first half of the number.
     id_is_invalid = False
+
+    if length_of_id % 2 == 1: # Get rid of all odd numbers as they can't have a repeated pattern in them.
+        return id_is_invalid
+
+    first_half_id_index = length_of_id // 2 - 1 # -1 for the first half of the number.
+
     for i in range(first_half_id_index + 1):
         if str_id[i] == str_id[i + first_half_id_index + 1]:
             id_is_invalid = True
         else:
             id_is_invalid = False
-            break
-    if id_is_invalid == True:
-        output += _id
-print(output)
+            return id_is_invalid
+
+    return id_is_invalid
+
+def main():
+    with open("day2/input.txt", 'r') as f:
+        exercise_input = f.read()
+
+    ranges_ids = [ranges.split('-') for ranges in exercise_input.split(',')]
+    all_ids = []
+
+    sum_of_invalid_ids = 0
+    for _range in ranges_ids:
+        for _id in range(int(_range[0]), int(_range[1]) + 1): 
+            if check_invalid_id(_id):
+                sum_of_invalid_ids += _id
+
+    print(sum_of_invalid_ids)
+    
+if __name__ =="__main__":
+    main()
+
+
