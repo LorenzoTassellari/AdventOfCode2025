dfbcc4add5b8f65a4279e7944ff1be1a05588e9e
Lorenzo Tassellari <m.tassellari@gmail.com>
2025-12-23 19:46:57 +0100

    Day 2 not working, made a start.

---
+++ day2/day2_part2.py
@@ -0,0 +1,58 @@
+
+def check_invalid_id(_id : int) -> bool:
+    '''
+    This check is now a bit harder, invalid ids can now be of any length and be repeated any number of times.
+    We still only need to keep the partial array up to the midpoint, as that's the last time the sequence could be
+    repeated.
+    Either way the whole sequence needs to be a repeat so 22224 still wouldn't work.
+    A sliding window solution sounds good here.
+    If the first number repeates then try keep going with a window of size 1. 
+    If that doesn't work, then go to a window of size 2 check for repeats.
+    Continue up to window of size len(id) // 2 - 1.
+    '''
+    print("here")
+    len_of_sliding_window = 1
+    id_str = str(_id)
+    id_len = len(id_str)
+    id_midpoint = id_len // 2 - 1
+    id_is_invalid = False
+    # Each time we increase the size of the window I'd like to restart from the beginning of the sequence.
+    # If the length of the window is larger than the midpoint of the id then we've gone too far and the
+    # sequence is valid.
+    while len_of_sliding_window <= id_midpoint: 
+        # If the length of the id is not divisible by the length of the window there's no point checking it.
+        if id_len % len_of_sliding_window != 0: 
+            len_of_sliding_window += 1
+            continue
+        print(len_of_sliding_window)
+        # I'll create an array with all the characters of the string, each string in the array will be the
+        # length of the window.
+        _array = []
+        for i in range(0, id_len, len_of_sliding_window):
+            print(i, id_len, len_of_sliding_window)
+            _array.append(id_str[i : i + len_of_sliding_window])
+            print(_array)
+        break
+
+    return id_is_invalid
+
+def main():
+    with open("day2/test.txt", 'r') as f:
+        exercise_input = f.read()
+
+    ranges_ids = [ranges.split('-') for ranges in exercise_input.split(',')]
+    all_ids = []
+
+    sum_of_invalid_ids = 0
+    for _range in ranges_ids:
+        for _id in range(int(_range[0]), int(_range[1]) + 1): 
+            print(_id)
+            if check_invalid_id(_id):
+                sum_of_invalid_ids += _id
+        break
+    print(sum_of_invalid_ids)
+    
+if __name__ =="__main__":
+    main()
+
+
