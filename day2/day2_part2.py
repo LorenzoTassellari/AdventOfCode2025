c85577ca4d9f34c25b419906f7a88b382a662b8f
Lorenzo Tassellari <m.tassellari@gmail.com>
2025-12-24 13:40:40 +0100

    Finished day 2 completely

--- day2/day2_part2.py
+++ day2/day2_part2.py
@@ -10,7 +10,6 @@ def check_invalid_id(_id : int) -> bool:
     If that doesn't work, then go to a window of size 2 check for repeats.
     Continue up to window of size len(id) // 2 - 1.
     '''
-    print("here")
     len_of_sliding_window = 1
     id_str = str(_id)
     id_len = len(id_str)
@@ -19,25 +18,36 @@ def check_invalid_id(_id : int) -> bool:
     # Each time we increase the size of the window I'd like to restart from the beginning of the sequence.
     # If the length of the window is larger than the midpoint of the id then we've gone too far and the
     # sequence is valid.
-    while len_of_sliding_window <= id_midpoint: 
+    while len_of_sliding_window <= id_midpoint + 1: 
+        skip_this_window = False
         # If the length of the id is not divisible by the length of the window there's no point checking it.
         if id_len % len_of_sliding_window != 0: 
             len_of_sliding_window += 1
             continue
-        print(len_of_sliding_window)
         # I'll create an array with all the characters of the string, each string in the array will be the
         # length of the window.
         _array = []
         for i in range(0, id_len, len_of_sliding_window):
-            print(i, id_len, len_of_sliding_window)
             _array.append(id_str[i : i + len_of_sliding_window])
-            print(_array)
-        break
+        first_item = _array[0]
+        for item in _array:
+            # Check that all the sections of the array are the same, this ensures the pattern is repeated.
+            # If at any point the pattern doens't work, then the ID is valid and check the next window.
+            if item != first_item:
+                len_of_sliding_window += 1
+                skip_this_window = True
+                break
+        if skip_this_window:
+            continue
 
+        # If we got here, that means that the patter was repeated, no need to check for other patterns just return True.
+        id_is_invalid = True
+        return id_is_invalid
+    # If we got here, that means there was no repeated pattern and the ID is valid. id_is_invalid should be False here.
     return id_is_invalid
 
 def main():
-    with open("day2/test.txt", 'r') as f:
+    with open("day2/input.txt", 'r') as f:
         exercise_input = f.read()
 
     ranges_ids = [ranges.split('-') for ranges in exercise_input.split(',')]
@@ -46,10 +56,8 @@ def main():
     sum_of_invalid_ids = 0
     for _range in ranges_ids:
         for _id in range(int(_range[0]), int(_range[1]) + 1): 
-            print(_id)
             if check_invalid_id(_id):
                 sum_of_invalid_ids += _id
-        break
     print(sum_of_invalid_ids)
     
 if __name__ =="__main__":
